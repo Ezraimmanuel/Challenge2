34bca56eaf30e3b965516b847ed5f4463c2c019d
Ezra <ezratudjan98@hotmail.com>
2018-03-20 11:03:05 +0100

    fix tweenmax

--- style/js/animation.js
+++ style/js/animation.js
@@ -1,5 +1,6 @@
 $(function() {
   // on load. Run al je functies hier
+  animate();
   fadeIn(".infocontainer", 1, "0px", "0px");
   fadeIn(".mars-container", 1, "0px", "-37.5px");
   fadeIn(".info", 1, "0px", "0px");
@@ -12,4 +13,15 @@ function fadeIn(element, opacity, margin, top, transform) {
     margin: margin,
     top: top,
   })
+}
+
+function animate() {
+  TweenMax.fromTo(".mars", 1, {
+    rotation: 0,
+  },
+  {
+    rotation: 360,
+    repeat: -1,
+    ease: Linear.easeNone,
+  }).timeScale(0.1);
 }
